457926d6c38ca99233c5a7921fa845831ced4aaf
Jason Poindexter <poindexter.json@gmail.com>
2021-05-02 10:57:36 -0700

    Basic fetch / response

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log('hello world')
+const URL = 'https://arweave.net/graphql'
+import * as fetch from 'node-fetch'
+
+export async function transactionById(ids: string[]): Promise<void> {
+  const query = `query($ids: [ID!]) {
+      transactions(ids: $ids) {
+          edges {
+              node {
+                  id
+              }
+          }
+      }
+  }`
+  const response = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables: { ids },
+    }),
+  })
+  if (response.status !== 200) {
+    throw Error(await response.text())
+  }
+  return await response.json()
+}
+
+;(async () => {
+  try {
+    console.log(await transactionById(['123']))
+  } catch (e) {
+    console.log(e)
+  }
+})()
